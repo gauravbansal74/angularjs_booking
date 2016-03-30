f2584dba4e4af6f34aa262f1424e2d3025ca3699
Gaurav Kumar <gauravbansal74@gmail.com>
2016-03-30 15:30:53 +0000

    new files modified

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,5 @@
  */
 angular.module('workspaceApp')
   .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+   
   });
